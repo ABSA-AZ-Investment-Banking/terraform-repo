29133c258e27255bed55d8a71941e86229d9280d
ShivaIndla <shiva.indla1519@gmail.com>
2025-11-01 00:22:15 +0530

    Siva indla/future branch (#13)
    * added jenkins file in projct
    
    * added stages in jenkins file
    
    * master to future branch (#10)
    
    * added jenkins file in projct (#9)
    
    Co-authored-by: Shivani-Indla <shiva.indla1518@gmail.com>
    
    * Update Jenkinsfile
    
    added stages to the jankins file directly to the master
    
    ---------
    
    Co-authored-by: Shivani-Indla <shiva.indla1518@gmail.com>
    
    * Update create-ec2.tf
    
    * Rename create-ec2.tf to main.tf
    
    * Update Jenkinsfile
    
    * main to future branch (#12)
    
    * added jenkins file in projct (#9)
    
    Co-authored-by: Shivani-Indla <shiva.indla1518@gmail.com>
    
    * Update Jenkinsfile
    
    added stages to the jankins file directly to the master
    
    * Update Jenkinsfile
    
    * Update and rename create-ec2.tf to main.tf
    
    * Siva indla/future branch (#11)
    
    * added jenkins file in projct
    
    * added stages in jenkins file
    
    * master to future branch (#10)
    
    * added jenkins file in projct (#9)
    
    Co-authored-by: Shivani-Indla <shiva.indla1518@gmail.com>
    
    * Update Jenkinsfile
    
    added stages to the jankins file directly to the master
    
    ---------
    
    Co-authored-by: Shivani-Indla <shiva.indla1518@gmail.com>
    
    * Update create-ec2.tf
    
    * Rename create-ec2.tf to main.tf
    
    * Update Jenkinsfile
    
    ---------
    
    Co-authored-by: Shivani-Indla <shiva.indla1518@gmail.com>
    
    ---------
    
    Co-authored-by: Shivani-Indla <shiva.indla1518@gmail.com>
    
    * Create locals.tf
    
    * Delete locals.tf
    
    * Create locals.tf
    
    * Create variables.tf
    
    ---------
    
    Co-authored-by: Shivani-Indla <shiva.indla1518@gmail.com>

---
+++ ec2/variables.tf
@@ -0,0 +1,10 @@
+variable "key-pair" {
+    type = string
+    description = "this key is for account 834988743121"
+    default = "shiva-practice-key"
+}
+variable "instance_type" {
+    type = string
+    description = "this instance type is for aws instances"
+    default = "t3.small"
+}
