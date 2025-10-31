5e66a61f3c0a4d6d87cf3735cfb98c7b0acf4f17
Shivani-Indla <shiva.indla1518@gmail.com>
2025-10-31 18:25:36 +0530

    updated Jenkinsfile

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.0.0-beta1"
+      version = "6.18.0"
     }
   }
 }
-#Provide authentication details for AWS
+
 provider "aws" {
   region = "us-east-1"
 }
